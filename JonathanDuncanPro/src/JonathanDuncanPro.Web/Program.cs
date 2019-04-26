718843e208027130f95b9f3aba0ab14a848ab63b
JonathanDuncan <duncanjonathan@yahoo.ca>
2019-04-25 22:39:24 -0600

    Adding and adapting blog files

--- JonathanDuncanPro/src/JonathanDuncanPro.Web/Program.cs
+++ JonathanDuncanPro/src/JonathanDuncanPro.Web/Program.cs
@@ -1,13 +1,67 @@
-﻿using Microsoft.AspNetCore;
+﻿using Core;
+using Core.Data;
+using Core.Services;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System.Threading;
 
 namespace JonathanDuncanPro.Web
 {
     public class Program
     {
+        private static CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<AppDbContext>();
+
+                try
+                {
+                    if (context.Database.GetPendingMigrations().Any())
+                    {
+                        context.Database.Migrate();
+                    }
+                }
+                catch { }
+
+                // load application settings from appsettings.json
+                var app = services.GetRequiredService<IAppService<AppItem>>();
+                AppConfig.SetSettings(app.Value);
+
+                if (app.Value.SeedData)
+                {
+                    var userMgr = (UserManager<AppUser>)services.GetRequiredService(typeof(UserManager<AppUser>));
+                    if (!userMgr.Users.Any())
+                    {
+                        userMgr.CreateAsync(new AppUser { UserName = "admin", Email = "admin@us.com" }, "admin").Wait();
+                        userMgr.CreateAsync(new AppUser { UserName = "demo", Email = "demo@us.com" }, "demo").Wait();
+                    }
+
+                    if (!context.BlogPosts.Any())
+                    {
+                        try
+                        {
+                            services.GetRequiredService<IStorageService>().Reset();
+                        }
+                        catch { }
+
+                        AppData.Seed(context);
+                    }
+                }
+            }
+
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
--- JonathanDuncanPro/src/JonathanDuncanPro.Web/Startup.cs
+++ JonathanDuncanPro/src/JonathanDuncanPro.Web/Startup.cs
@@ -1,33 +1,74 @@
-﻿using Autofac;
+﻿using Askmethat.Aspnet.JsonLocalizer.Extensions;
+using Autofac;
 using Autofac.Extensions.DependencyInjection;
+using Core;
+using Core.Api;
+using Core.Data;
+using Core.Extensions;
 using JonathanDuncanPro.Core.SharedKernel;
 using JonathanDuncanPro.Infrastructure.Data;
 using JonathanDuncanPro.Web.Code.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+//using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 using Swashbuckle.AspNetCore.Swagger;
 using System;
+using System.Globalization;
 using System.Reflection;
+using AppDbContext = Core.Data.AppDbContext;
+
+
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+
+using System.IO;
+using Microsoft.Extensions.Logging;
 
 namespace JonathanDuncanPro.Web
 {
     public class Startup
     {
+        private string _projectRootFolder;
+
         public Startup(IConfiguration config)
         {
             Configuration = config;
+
+            Log.Logger = new LoggerConfiguration()
+             .Enrich.FromLogContext()
+             .WriteTo.RollingFile("Logs/{Date}.txt", LogEventLevel.Warning)
+             .CreateLogger();
         }
 
         public IConfiguration Configuration { get; }
 
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
+            var section = Configuration.GetSection("Blogifier");
+
+            services.AddAppSettings<AppItem>(section);
+
+            if (section.GetValue<string>("DbProvider") == "SqlServer")
+            {
+                AppSettings.DbOptions = options => options.UseSqlServer(section.GetValue<string>("ConnString"));
+            }
+            else if (section.GetValue<string>("DbProvider") == "MySql")
+            {
+                AppSettings.DbOptions = options => options.UseMySql(section.GetValue<string>("ConnString"));
+            }
+            else
+            {
+                AppSettings.DbOptions = options => options.UseSqlite(section.GetValue<string>("ConnString"));
+            }
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
@@ -38,6 +79,20 @@ namespace JonathanDuncanPro.Web
             string dbName = Guid.NewGuid().ToString();
             services.AddDbContext<AppDbContext>(options =>
                 options.UseInMemoryDatabase(dbName));
+            //services.AddDbContext<AppDbContext>(options =>
+            //    options.UseInMemoryDatabase(dbName));
+            services.AddDbContext<AppDbContext>(AppSettings.DbOptions, ServiceLifetime.Transient);
+
+            services.AddIdentity<AppUser, IdentityRole>(options => {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 4;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.User.AllowedUserNameCharacters = null;
+            })
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
 
             services.AddLogging(loggingBuilder =>
             {
@@ -45,12 +100,45 @@ namespace JonathanDuncanPro.Web
                 loggingBuilder.AddConsole();
                 loggingBuilder.AddDebug();
             });
-            //options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            //TODO from Iblogger
+            //services.AddLogging(loggingBuilder =>
+            //    loggingBuilder.AddSerilog(dispose: true));
+
+            services.AddJsonLocalization();
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var supportedCultures = new[]
+                {
+                    new CultureInfo("en-US"),
+                    new CultureInfo("ru-RU"),
+                    new CultureInfo("zh-cn"),
+                    new CultureInfo("zh-tw")
+                };
+
+                options.DefaultRequestCulture = new RequestCulture(culture: "en-US", uiCulture: "en-US");
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+            });
+
+            services.AddRouting(options => options.LowercaseUrls = true);
 
             services.AddMvc()
+                 .AddViewLocalization()
+                  .ConfigureApplicationPartManager(p =>
+                  {
+                      foreach (var assembly in AppConfig.GetAssemblies())
+                      {
+                          p.ApplicationParts.Add(new AssemblyPart(assembly));
+                      }
+                  })
+                  .AddRazorPagesOptions(options =>
+                  {
+                      options.Conventions.AuthorizeFolder("/Admin");
+                  })
+                    .AddApplicationPart(typeof(AuthorsController).GetTypeInfo().Assembly).AddControllersAsServices()
                 .AddControllersAsServices()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-           
+
             services.AddWebOptimizer();
 
             services.AddSwaggerGen(c =>
@@ -58,6 +146,17 @@ namespace JonathanDuncanPro.Web
                 c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
             });
 
+            // TODO from Iblogger
+            //services.AddSwaggerGen(setupAction => {
+            //    setupAction.SwaggerDoc("spec",
+            //        new Microsoft.OpenApi.Models.OpenApiInfo()
+            //        {
+            //            Title = "Blogifier API",
+            //            Version = "1"
+            //        });
+            //    setupAction.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "CoreAPI.xml"));
+            //});
+            services.AddAppServices();
             return BuildDependencyInjectionProvider(services);
         }
 
@@ -80,6 +179,12 @@ namespace JonathanDuncanPro.Web
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+
+            _projectRootFolder = env.ContentRootPath.Substring(0,
+                env.ContentRootPath.LastIndexOf(@"\ProjectRoot\", StringComparison.Ordinal) + @"\ProjectRoot\".Length);
+
+            AppSettings.ContentRootPath = _projectRootFolder;
+            Console.Write(_projectRootFolder);
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -116,6 +221,10 @@ namespace JonathanDuncanPro.Web
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                name: "blog",
+                template: "{area:exists}/{controller=Blog}/{action=Index}/{id?}");
+
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
 
@@ -124,3 +233,4 @@ namespace JonathanDuncanPro.Web
         }
     }
 }
+
