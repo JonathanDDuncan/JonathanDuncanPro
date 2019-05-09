0b2bb874685f6023b9661b03978570c87dad69b2
JonathanDuncan <duncanjonathan@yahoo.ca>
2019-05-08 19:36:47 -0600

    Add canonical links to pages

---
+++ JonathanDuncanPro/src/JonathanDuncanPro.Web/HttpContextExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace JonathanDuncanPro.Web
+{
+    public static class HttpContextExtensions
+    {
+        public static string CanonicalUrl(this HttpContext context)
+        {
+            var host = context.Request.Host.Host;
+            var port = context.Request.Host.Port ?? 0;
+
+            var pathBase = context.Request.PathBase.HasValue
+                ? context.Request.PathBase.Value
+                : string.Empty;
+
+            var path = pathBase + context.Request.Path.Value;
+
+            var query = context.Request.QueryString.HasValue
+                ? $"{context.Request.QueryString.Value}"
+                : string.Empty;
+
+            return MakeCanonicalUrl(host, port, path, query);
+        }
+
+        public static string MakeCanonicalUrl(string host, int port, string path, string query)
+        {
+            var hostnoprotocol = host.ToLowerInvariant().Replace("https://", "").Replace("http://", "");
+            hostnoprotocol = (!hostnoprotocol.StartsWith("localhost") && !hostnoprotocol.StartsWith("www")) ? "www." + hostnoprotocol : hostnoprotocol;
+            var portString = port == 80 || port == 443 || port == 0
+                ? string.Empty
+                : $":{port}";
+
+            var pathLower = path.ToLower();
+
+            if (pathLower != "/" && pathLower.EndsWith('/'))
+            {
+                pathLower = pathLower.TrimEnd('/');
+            }
+            var querywithquestion = string.IsNullOrEmpty(query) ? query : (query.StartsWith('?') ? query : "?" + query);
+
+            const string httpsFormat = "https://{0}{1}{2}{3}";
+            return string.Format(httpsFormat, hostnoprotocol, portString, pathLower, querywithquestion);
+        }
+    }
+}
---
+++ JonathanDuncanPro/tests/JonathanDuncanPro.Tests/Integration/Web/CanonicalURLShould.cs
@@ -0,0 +1,34 @@
+﻿using JonathanDuncanPro.Core.Entities;
+using JonathanDuncanPro.Web;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace JonathanDuncanPro.Tests.Integration.Web
+{
+
+    public class CanonicalURL
+    {
+      
+        [Theory]
+        [InlineData("https://www.jonathanduncan.pro", 0, @"/home/about", "", "https://www.jonathanduncan.pro/home/about")]
+        [InlineData("https://www.jonathanduncan.pro", 0, @"/home/about/", "", "https://www.jonathanduncan.pro/home/about")]
+        [InlineData("http://www.jonathanduncan.pro", 0, @"/home/about", "", "https://www.jonathanduncan.pro/home/about")]
+        [InlineData("http://jonathanduncan.pro", 0, @"/home/about", "", "https://www.jonathanduncan.pro/home/about")]
+        [InlineData("http://www.jonathanduncan.pro", 0, @"/home/about/", "", "https://www.jonathanduncan.pro/home/about")]
+        [InlineData("http://jonathanduncan.pro", 0, @"/home/about/", "", "https://www.jonathanduncan.pro/home/about")]
+        [InlineData("http://jonathanduncan.pro", 0, @"/home/about/", "abc=12345", "https://www.jonathanduncan.pro/home/about?abc=12345")]
+        [InlineData("http://jonathanduncan.pro", 0, @"/home/about/", "?abc=12345", "https://www.jonathanduncan.pro/home/about?abc=12345")]
+        [InlineData("http://localhost", 5000, @"/home/about/", "", "https://localhost:5000/home/about")]
+        [InlineData("http://localhost", 5000, @"/home/about/", "abc=12345", "https://localhost:5000/home/about?abc=12345")]
+        [InlineData("http://localhost", 5000, @"/home/about/", "?abc=12345", "https://localhost:5000/home/about?abc=12345")]
+        public async Task CanonicalUrlTest(string host, int port, string path, string query, string expected)
+        {
+           var result = HttpContextExtensions.MakeCanonicalUrl(host, port, path, query);
+            Assert.Equal(expected, result);
+        }
+    }
+}
